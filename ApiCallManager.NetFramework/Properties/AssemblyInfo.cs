9d9194885877059b85cd9162cefa51d9982ece60
farhad sadeghi <farhad.iust.ac@gmail.com>
2023-12-10 12:30:00 +0330

    Versions 6.0.0 and 4.8.0 Published on Nuget. (1402.09.19)

--- ApiCallManager.NetFramework/Properties/AssemblyInfo.cs
+++ ApiCallManager.NetFramework/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ApiCallManager.NetFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("REST API call manager.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ApiCallManager.NetFramework")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.8.0.0")]
+[assembly: AssemblyFileVersion("4.8.0.0")]
